46c9a761e485599e9d172929e797ad7133954024
Eetu Soronen <soronen@users.noreply.github.com>
2023-01-31 12:49:14 +0200

    these tests are worthless, but they pass

--- src/test/java/model/ScoreboardTest.java
+++ src/test/java/model/ScoreboardTest.java
@@ -1,2 +1,54 @@
-package model;public class ScoreboardTest {
+package model;
+
+import database.dao.ILeaderboardDAO;
+import database.dao.LeaderboardDAO;
+import database.datasource.SqlJpaConn;
+import database.entity.Leaderboard;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ScoreboardTest {
+
+    //before all method that create entitymanager to save load time
+    @BeforeAll
+    static void setUpAll() {
+        EntityManager em = SqlJpaConn.getInstance();
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getInstance() {
+        assertNotNull(Scoreboard.getInstance());
+    }
+
+    @Test
+    void getWorldscores() {
+        Scoreboard scoreboard = Scoreboard.getInstance();
+        ArrayList<Leaderboard> worldscores = scoreboard.getWorldscores();
+        // assert that worldscore containts at least 1 leaderboard object
+        assertNotNull(worldscores.get(0));
+    }
+
+    @Test
+    void refreshWorldScores() {
+        Scoreboard scoreboard = Scoreboard.getInstance();
+        scoreboard.refreshWorldScores();
+        ArrayList<Leaderboard> worldscores = scoreboard.getWorldscores();
+        // assert that worldscore containts at least 1 leaderboard object
+        assertNotNull(worldscores.get(0));
+    }
 }
