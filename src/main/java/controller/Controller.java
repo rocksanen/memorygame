5d126f94f8f9f70ed9f2ee36ba4c2333fe35adf9
rocksanen <rocksanen@gmail.com>
2023-02-03 14:46:35 +0200

    Simple ui for easymode testing

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -1,5 +1,94 @@
 package controller;
 
-public class Controller implements IController{
+import javafx.application.Platform;
+import model.Engine;
+import model.IEngine;
+import model.MemoryObject;
+import model.ModeType;
+import visuals.IGui;
+
+import java.util.ArrayList;
+
+public class Controller implements IControllerVtoE, IControllerEtoV {
+
+    private final IGui ui;
+    private IEngine engine;
+    public Controller(IGui ui) {
+
+        this.ui = ui;
+
+    }
+
+    @Override
+    public void startEasyGame() {
+
+        this.engine = new Engine(ModeType.EASY,this);
+        this.engine.setMemoryObjects();
+
+    }
+    @Override
+    public void eB0(int i){
+
+        engine.addToComparing(i);
+
+    }
+    @Override
+    public void eB1(int i){
+
+        engine.addToComparing(i);
+
+    }
+    @Override
+    public void eB2(int i){
+
+        engine.addToComparing(i);
+
+    }
+    @Override
+    public void eB3(int i){
+
+        engine.addToComparing(i);
+
+    }
+    @Override
+    public void eB4(int i){
+
+        engine.addToComparing(i);
+
+    }
+    @Override
+    public void eB5(int i) {
+
+        engine.addToComparing(i);
+
+    }
+
+    @Override
+    public void sendType(int id,int typeID) {
+
+       ui.setTypeToLabel(id,typeID);
+
+    }
+
+    @Override
+    public void clearPair(ArrayList<MemoryObject> memoryList){
+
+        Platform.runLater(() ->ui.clearPair(memoryList));
+
+    }
+
+    @Override
+    public void setWorldScore(ArrayList<String> worldScoreList) {
+
+       Platform.runLater(() -> ui.setWorldScore(worldScoreList));
+    }
+
+    @Override
+    public void setPersonalScore(ArrayList<String> personalScoreList) {
+
+        Platform.runLater(() -> ui.setPersonalScores(personalScoreList));
+
+    }
+
 
 }
--- src/main/java/visuals/Gui.java
+++ src/main/java/visuals/Gui.java
@@ -1,4 +1,195 @@
 package visuals;
 
-public class Gui implements IGui{
+import controller.Controller;
+import controller.IControllerVtoE;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.PhongMaterial;
+import javafx.stage.Stage;
+import model.MemoryObject;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
+
+public class Gui extends Application implements IGui{
+
+    private final IControllerVtoE controller = new Controller(this);
+    private final String EASYMODE = "/visuals/easymode.fxml";
+
+    private ArrayList<MemoryObject> memo;
+    private ObservableList<String> worldObservable;
+    private ObservableList<String> personObservable;
+
+    Stage primaryStage;
+
+    //@FXML Box e0,e1,e2,e3,e4,e5;
+
+    @FXML Button startEasyGame;
+    @FXML
+    Button eB0;
+    @FXML
+    Button eB1;
+    @FXML
+    Button eB2;
+    @FXML
+    Button eB3;
+    @FXML
+    Button eB4;
+    @FXML
+    Button eB5;
+
+    @FXML
+    Label eL0;
+    @FXML
+    Label eL1;
+    @FXML
+    Label eL2;
+    @FXML
+    Label eL3;
+    @FXML
+    Label eL4;
+    @FXML
+    Label eL5;
+    @FXML
+    Pane pane;
+    @FXML PhongMaterial material;
+    @FXML
+    GridPane easyGrid;
+    @FXML
+    ListView<String> personalScores;
+    @FXML
+    ListView<String> worldScores;
+
+    private ArrayList<Label> labelList;
+
+    public static void main(String[] args) {launch(args);}
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        this.primaryStage = primaryStage;
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(EASYMODE)));
+        Scene scene = new Scene(root,1150,700);
+        this.primaryStage.setScene(scene);
+        this.primaryStage.setFullScreenExitHint ("");
+        this.primaryStage.setResizable(true);
+        this.primaryStage.show();
+
+    }
+    @Override
+    public void init() {
+
+        eB0 = new Button();
+        eB1 = new Button();
+        eB2 = new Button();
+        eB3 = new Button();
+        eB4 = new Button();
+        eB5 = new Button();
+
+        eL0 = new Label();
+        eL1 = new Label();
+        eL2 = new Label();
+        eL3 = new Label();
+        eL4 = new Label();
+        eL5 = new Label();
+
+        personalScores = new ListView<>();
+        worldScores = new ListView<>();
+
+        worldObservable = FXCollections.observableArrayList();
+        personObservable = FXCollections.observableArrayList();
+
+        startEasyGame = new Button();
+
+    }
+
+    @FXML
+    public void eButton0() {controller.eB0(0);}
+    @FXML
+    public void eButton1() {controller.eB1(1);}
+    @FXML
+    public void eButton2() {controller.eB2(2);}
+    @FXML
+    public void eButton3() {controller.eB3(3);}
+    @FXML
+    public void eButton4() {controller.eB4(4);}
+    @FXML
+    public void eButton5() {controller.eB5(5);}
+
+    @FXML
+    public void setStartEasyGame() {
+
+        clearTable();
+        controller.startEasyGame();
+    }
+
+    public void setTypeToLabel(int id,int typeID) {
+
+        switch (id) {
+            case 0 -> eL0.setText(Integer.toString(typeID));
+            case 1 -> eL1.setText(Integer.toString(typeID));
+            case 2 -> eL2.setText(Integer.toString(typeID));
+            case 3 -> eL3.setText(Integer.toString(typeID));
+            case 4 -> eL4.setText(Integer.toString(typeID));
+            case 5 -> eL5.setText(Integer.toString(typeID));
+        }
+    }
+
+    public void clearTable(){
+
+        eL0.setText("");
+        eL1.setText("");
+        eL2.setText("");
+        eL3.setText("");
+        eL4.setText("");
+        eL5.setText("");
+
+    }
+
+    public void clearPair(ArrayList<MemoryObject> memoryList){
+
+        for(MemoryObject obj: memoryList) {
+            switch (obj.getIdNumber()) {
+                case 0 -> eL0.setText("");
+                case 1 -> eL1.setText("");
+                case 2 -> eL2.setText("");
+                case 3 -> eL3.setText("");
+                case 4 -> eL4.setText("");
+                case 5 -> eL5.setText("");
+            }
+        }
+    }
+
+    @Override
+    public void setWorldScore(ArrayList<String> worldList) {
+
+
+        // Create an observable list from the worldList
+        worldObservable = FXCollections.observableArrayList();
+
+        // Add all the elements from the worldList to the worldObservable
+        worldObservable.addAll(worldList);
+
+        // Add all the elements from the worldObservable to the worldScores list
+        worldScores.getItems().addAll(worldObservable);
+
+    }
+
+    @Override
+    public void setPersonalScores(ArrayList<String> personalList) {
+
+        personObservable.addAll(personalList);
+        personalScores = new ListView<>(personObservable);
+
+    }
 }
