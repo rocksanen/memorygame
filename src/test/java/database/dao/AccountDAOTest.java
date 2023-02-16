a9fed19f356c064c2a75ec814f142dde9ac7e610
tahasafdari <tahahasan.safdari@gmail.com>
2023-02-16 15:29:45 +0200

    database test classes all added.

--- src/test/java/database/dao/AccountDAOTest.java
+++ src/test/java/database/dao/AccountDAOTest.java
@@ -1,38 +1,80 @@
 package database.dao;
 
+import database.entity.Account;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountDAOTest {
 
+    private AccountDAO accountDAO;
+
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception {
+        accountDAO = new AccountDAO();
     }
 
     @AfterEach
-    void tearDown() {
+    void tearDown() throws Exception{
+        accountDAO = null;
     }
 
     @Test
     void saveAccount() {
+        Account account = new Account();
+        account.setUsername("testUser");
+        account.setPassword("testPassword");
+        assertTrue(accountDAO.saveAccount(account));
     }
 
     @Test
     void getAccount() {
+        Account acc = new Account();
+        acc.setUsername("testUser");
+        acc.setPassword("testPass");
+        accountDAO.saveAccount(acc);
+        assertNotNull(accountDAO.getAccount(acc.getAccountid()));
+
     }
 
     @Test
     void getAccountByName() {
+        Account acc = new Account();
+        acc.setUsername("userTest");
+        accountDAO.saveAccount(acc);
+        assertNotNull(accountDAO.getAccountByName("userTest"));
+
     }
 
     @Test
     void getAllAccounts() {
+        Account acc1 = new Account("testuser5", "password5");
+        Account acc2 = new Account("testuser6", "password6");
+
+        accountDAO.saveAccount(acc1);
+        accountDAO.saveAccount(acc2);
+
+        ArrayList<Account> accounts = accountDAO.getAllAccounts();
+        assertTrue(accounts.contains(acc1));
+        assertTrue(accounts.contains(acc2));
     }
 
     @Test
     void deleteAccount() {
+        // check that the account exists in the database
+        Account retrievedAccount = accountDAO.getAccountByName("testuser5");
+        assertNotNull(retrievedAccount);
+
+        // delete the account
+        boolean deleteResult = accountDAO.deleteAccount(retrievedAccount.getAccountid());
+        assertTrue(deleteResult);
+
+        // check that the account no longer exists in the database
+        retrievedAccount = accountDAO.getAccountByName("testUser");
+        assertNull(retrievedAccount);
     }
 }
--- src/test/java/database/dao/LeaderboardDAOTest.java
+++ src/test/java/database/dao/LeaderboardDAOTest.java
@@ -1,42 +1,110 @@
 package database.dao;
 
+import database.entity.Account;
+import database.entity.Leaderboard;
+import model.ModeType;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class LeaderboardDAOTest {
 
+    private static LeaderboardDAO leaderboardDAO;
+
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception {
+        leaderboardDAO = new LeaderboardDAO();
+
     }
 
     @AfterEach
-    void tearDown() {
+    void tearDown() throws Exception {
+        leaderboardDAO = null;
     }
 
+    //Test case for saving a score to the database
     @Test
     void saveScore() {
+        // Create a test account
+        AccountDAO  accountDAO = new AccountDAO();
+        Account account = new Account("test1", "test1");
+        accountDAO.saveAccount(account);
+
+
+        // Create a test leaderboard entry
+        Double time = 10.5;
+        int points = 100;
+        ModeType difficulty = ModeType.EASY;
+        Date timestamp = new Date();
+        Leaderboard leaderboard = new Leaderboard(account, time, points, difficulty, timestamp);
+
+        // Attempt to save the leaderboard entry
+        boolean saved = leaderboardDAO.saveScore(leaderboard);
+
+        // Check that the leaderboard entry was saved successfully
+        assertTrue(saved);
+
     }
 
     @Test
     void getAccountScores() {
+        LeaderboardDAO dao = new LeaderboardDAO();
+        ArrayList<Leaderboard> scores = dao.getAccountScores(1L);
+        assertTrue(scores instanceof ArrayList);
+
     }
 
     @Test
     void getAccountScoresByDifficulty() {
+        // Call the DAO to retrieve scores for a specific account ID and difficulty level
+        Long accountid = 1L; // Replace with a valid account ID from your database
+        ModeType difficulty = ModeType.EASY;
+        LeaderboardDAO dao = new LeaderboardDAO();
+        ArrayList<Leaderboard> scores = dao.getAccountScoresByDifficulty(accountid, difficulty);
+
+        // Check that the result list is not null
+        assertNotNull(scores);
+
     }
 
     @Test
     void readWorldScores() {
+        LeaderboardDAO dao = new LeaderboardDAO();
+        ArrayList<Leaderboard> scores = dao.readWorldScores(ModeType.EASY);
+        assertTrue(scores instanceof ArrayList<Leaderboard>);
+        assertTrue(scores.get(0) instanceof Leaderboard);
     }
 
     @Test
     void deleteScore() {
+        Long scoreId = 11L;
+        boolean result = leaderboardDAO.deleteScore(scoreId);
+        Assertions.assertTrue(result);
     }
 
     @Test
     void deleteAllScores() {
+        // Call the DAO to delete all scores for a specific account ID
+        Long accountid = 9L;
+        LeaderboardDAO dao = new LeaderboardDAO();
+
+        // Get all the scores for the specified account ID
+        ArrayList<Leaderboard> scores = dao.getAccountScores(accountid);
+
+        // Delete each score
+        for (Leaderboard score : scores) {
+            dao.deleteScore(score.getScoreid());
+        }
+
+        // Check that there are no scores left for the specified account ID
+        ArrayList<Leaderboard> remainingScores = dao.getAccountScores(accountid);
+        assertEquals(0, remainingScores.size());
+
     }
 }
