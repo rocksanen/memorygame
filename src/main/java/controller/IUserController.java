a7108b88597460b47b29138d4fb7a5f2289c060b
Eetu Soronen <soronen@users.noreply.github.com>
2023-04-24 17:47:41 +0300

    javadoc and cleanup

--- src/main/java/controller/IUserController.java
+++ src/main/java/controller/IUserController.java
@@ -2,14 +2,41 @@ package controller;
 
 public interface IUserController {
 
+    /**
+     * returns the score for the next wrong guess
+     *
+     * @param username the username of the user
+     * @param password the password of the user
+     * @return true if the user was successfully logged in
+     */
     boolean login(String username, String password);
 
+    /**
+     * registers a new user
+     *
+     * @param username the username of the new user
+     * @param password the password of the new user
+     * @return true if the user was successfully registered
+     */
     boolean register(String username, String password);
 
+    /**
+     * logs out the user if logged in
+     */
     void logout();
 
+    /**
+     * returns true if the user is logged in
+     *
+     * @return true if the user is logged in
+     */
     boolean isLoggedIn();
 
+    /**
+     * returns the username of the logged-in user
+     *
+     * @return the username of the logged-in user
+     */
     String getUsername();
 
 }
