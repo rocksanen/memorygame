9d7b8aee191ef3a47b5ec0c63f2ea7dfbf694cac
tahasafdari <tahahasan.safdari@gmail.com>
2023-05-05 12:39:05 +0300

    javadoc and comments added for JavaFXInternationalization class

--- src/main/java/visuals/internationalization/JavaFXInternationalization.java
+++ src/main/java/visuals/internationalization/JavaFXInternationalization.java
@@ -1,20 +1,37 @@
+/**
+ The JavaFXInternationalization class handles the internationalization of the application by managing the current locale
+ and loading the corresponding resource bundle file. It provides methods for setting and retrieving the current locale.
+ */
 package visuals.internationalization;
-
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class JavaFXInternationalization {
-    private static Locale locale = new Locale("en");
 
+    // The default locale is English
+    private static Locale locale = new Locale("en");
+    /**
+     * Retrieves the resource bundle for the current locale.
+     *
+     * @return A ResourceBundle object containing the key-value pairs for the current locale.
+     */
     public static ResourceBundle internationalizationLoaderProperties() {
 
         return ResourceBundle.getBundle("Bundle", locale);
     }
-
+    /**
+     * Sets the current locale of the application to the specified locale.
+     *
+     * @param newLocale The Locale object representing the new locale.
+     */
     public static void setLocale(Locale newLocale){
         locale = newLocale;
     }
-
+    /**
+     * Retrieves the current locale of the application.
+     *
+     * @return The Locale object representing the current locale.
+     */
     public static Locale getLocale(){
         return locale;
     }
